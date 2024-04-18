dcf1baf393f97b1cbe9a52cab3ba56bfc4e79cd9
karimalsaka <karimalsaka@hotmail.com>
2024-04-18 10:06:56 +0100

    implement SignInWithApple

--- MyApp/Authentication/AuthManager.swift
+++ MyApp/Authentication/AuthManager.swift
@@ -1,5 +1,7 @@
 import Foundation
+import AuthenticationServices
 import FirebaseAuth
+import CryptoKit
 
 struct AuthUserModel {
     let id: String
@@ -13,7 +15,7 @@ struct AuthUserModel {
     }
 }
 
-final class AuthManager: Equatable {
+final class AuthManager: NSObject {
     static func == (lhs: AuthManager, rhs: AuthManager) -> Bool {
         return true
     }
@@ -28,6 +30,18 @@ final class AuthManager: Equatable {
     func signIn(email: String, password: String) async throws -> AuthUserModel {
         let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
         return AuthUserModel(user: authDataResult.user)
+    }
+    
+    @discardableResult
+    func signInWithApple(tokens: SignInWithAppleResult) async throws -> AuthUserModel {
+        let credential = OAuthProvider.credential(withProviderID: "apple.com" , idToken: tokens.token, rawNonce: tokens.nonce)
+        return try await signIn(credential: credential)
+    }
+    
+    func signIn(credential: AuthCredential) async throws -> AuthUserModel {
+        let authDataResult = try await Auth.auth().signIn(with: credential)
+        let model = AuthUserModel(user: authDataResult.user)
+        return model
     }
     
     func signOut() throws {
@@ -45,4 +59,12 @@ final class AuthManager: Equatable {
     func isUserSignedIn() -> Bool {
         return  Auth.auth().currentUser != nil
     }
+}
+
+struct SignInWithAppleResult {
+    let token: String
+    let nonce: String
+}
+
+extension AuthManager: ASAuthorizationControllerDelegate {
 }
--- MyApp/Authentication/Views/AuthenticationOptionsView.swift
+++ MyApp/Authentication/Views/AuthenticationOptionsView.swift
@@ -1,12 +1,101 @@
 import SwiftUI
+import AuthenticationServices
+import CryptoKit
 
 @MainActor
-final class AuthenticationOptionsViewModel: ObservableObject {
+final class AuthenticationOptionsViewModel:NSObject, ObservableObject {
     var authManager: AuthManager
-    
+    var didSignInWithAppleSuccessfully = false
+
+    private var currentNonce: String?
+    private var presentationAnchor: ASPresentationAnchor?
+
     init(authManager: AuthManager) {
         self.authManager = authManager
     }
+    
+    func startSignInWithAppleFlow(presentationAnchor: ASPresentationAnchor?) {
+        guard let presentationAnchor else {
+            return
+        }
+        self.presentationAnchor = presentationAnchor
+        
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+}
+
+extension AuthenticationOptionsViewModel: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return presentationAnchor!
+    }
+}
+
+extension AuthenticationOptionsViewModel: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential,
+              let appleIDToken = appleIDCredential.identityToken,
+              let idTokenString = String(data: appleIDToken, encoding: .utf8),
+              let nonce = currentNonce else {
+            print("error")
+            return
+        }
+        
+        let tokens = SignInWithAppleResult(token: idTokenString, nonce: nonce)
+        
+        Task {
+            try await authManager.signInWithApple(tokens: tokens)
+            didSignInWithAppleSuccessfully = true
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+    }
+
+    //MARK: - Helper functions
+    
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      var randomBytes = [UInt8](repeating: 0, count: length)
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+      if errorCode != errSecSuccess {
+        fatalError(
+          "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+        )
+      }
+
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+
+      let nonce = randomBytes.map { byte in
+        charset[Int(byte) % charset.count]
+      }
+
+      return String(nonce)
+    }
+    
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+
 }
 
 struct AuthenticationOptionsView: View {
@@ -23,8 +112,8 @@ struct AuthenticationOptionsView: View {
                     
                     // replace with image lol
                     Rectangle()
+                        .frame(height: 300)
                         .foregroundStyle(Color.cyan)
-                        .frame(width: .infinity, height: 300)
                         .clipShape(RoundedRectangle(cornerRadius: 20))
                         .padding(.top, 30)
                         .padding(.bottom, 50)
@@ -66,11 +155,24 @@ struct AuthenticationOptionsView: View {
                             .cornerRadius(10)
                     }
                      
+                    Button {
+                        viewModel.startSignInWithAppleFlow(presentationAnchor: coordinator.navigationController.topViewController?.view.window)
+                    } label: {
+                        AppleButtonView(type: .continue, style: .black)
+                            .allowsHitTesting(false)
+                            .frame(height: 55)
+                    }
+                    
                      Spacer()
                 }
             }
             .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onChange(of: viewModel.didSignInWithAppleSuccessfully) { success in
+                if success {
+                    coordinator.userSignedIn()
+                }
+            }
     }
 }
 
@@ -79,4 +181,17 @@ struct AuthenticationOptionsView: View {
     let viewModel = AuthenticationOptionsViewModel(authManager: authManager)
 
     return AuthenticationOptionsView(viewModel: viewModel)
+}
+
+struct AppleButtonView: UIViewRepresentable {
+    let type: ASAuthorizationAppleIDButton.ButtonType
+    let style: ASAuthorizationAppleIDButton.Style
+    
+    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+        let authorization = ASAuthorizationAppleIDButton(authorizationButtonType: type, authorizationButtonStyle: style)
+        return authorization
+    }
+    
+    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
+    }
 }
